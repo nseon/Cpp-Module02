162c91743de8b3ef43ba5cf73f41cc1ef302942b
nseon <nseon@student.42lyon.fr>
2025-11-07 11:09:01 +0100

    ex03 in  progress

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,30 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 10:54:29 by nseon             #+#    #+#             */
-/*   Updated: 2025/11/06 10:54:29 by nseon            ###   ########.fr       */
+/*   Updated: 2025/11/07 11:06:58 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Point.hpp"
+
+Point::Point() : _x(0), _y(0)
+{}
+
+Point::Point(float const x, float const y) : _x(x), _y(y)
+{}
+
+Point::Point(const Point &model) : _x(model._x), _y(model._y)
+{}
+
+Point::~Point()
+{}
+
+Fixed Point::getX() const
+{
+	return (_x);
+}
+
+Fixed Point::getY() const
+{
+	return (_y);
+}
--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Point.hpp                                          :+:      :+:    :+:   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/06 10:54:26 by nseon             #+#    #+#             */
-/*   Updated: 2025/11/07 11:07:03 by nseon            ###   ########.fr       */
+/*   Created: 2025/11/06 14:20:30 by nseon             #+#    #+#             */
+/*   Updated: 2025/11/07 11:06:10 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
+#include "Point.hpp"
 
-class Point {
-	private:
-		Fixed const _x;
-		Fixed const _y;
-	public:
-		Point();
-		Point(float const x, float const y);
-		Point(const Point &model);
-		~Point();
-		
-		Fixed getX() const;
-		Fixed getY() const;
-};
+Fixed barycentre (Point const p1, Point const p2, Point const p3)
+{
+    return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
+}
+
+bool bsp( Point const a, Point const b, Point const c, Point const point)
+{
+
+}
